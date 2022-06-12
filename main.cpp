94e8d5e48482c06c40e5e84e55c218452fa05240
bshawnee <famousman204@yandex.ru>
2022-06-12 14:39:29 +0300

    added arguments

--- main.cpp
+++ main.cpp
@@ -2,12 +2,113 @@
 #include <SFML/Graphics.hpp>
 #include "Game.h"
 #include <unistd.h>
+#include <fstream>
+#include <map>
+#include <boost/algorithm/string.hpp>
 constexpr std::size_t OutlineThickness = 12;
 
+std::map<std::string, int> argumentSettings(const char **argv)
+{
+    std::map<std::string, int> settings;
+    for (int i = 0; argv[i] != nullptr; i++) {
+        std::string argument(argv[i]);
+        boost::algorithm::to_lower(argument);
+        if (argument == "-size") {
+            i++;
+            int tmp;
+            if (argv[i] == nullptr) {
+                throw std::logic_error("argument '-size' is not specified\n");
+            }
+            else {
+                try {
+                    tmp = std::stoi(argv[i]);
+                    if (tmp <= 0) {
+                        throw std::exception();
+                    }
+                } catch (std::exception&) {
+                    throw std::logic_error("argument '-size' is not specified\n");
+                }
+            }
+            settings["sizeX"] = tmp;
 
-int main() {
+            i++;
+            if (argv[i] == nullptr) {
+                throw std::logic_error("argument '-size' is not specified\n");
+            }
+            else {
+                try {
+                    tmp = std::stoi(argv[i]);
+                    if (tmp <= 0) {
+                        throw std::exception();
+                    }
+                } catch (std::exception&) {
+                    throw std::logic_error("argument '-size' is not specified\n");
+                }
+            }
+            settings["sizeY"] = tmp;
+        }
+        else if (argument == "-solvable") {
+            i++;
+            std::string_view solvable(argv[i]);
+            int tmp;
+            if (solvable.empty()) {
+                throw std::logic_error("argument '-solvable' is not specified\n");
+            }
+            try {
+                tmp = std::stoi(solvable.data());
+            }
+            catch (std::exception&) {
+                throw std::logic_error("argument '-solvable' is not specified\n");
+            }
+            settings["solvable"] = static_cast<bool>(tmp);
+        }
+        else if (argument == "-aimode") {
+            settings["aimode"] = 1;
+        }
+    }
+    if (settings.find("sizeX") == settings.end()) {
+        settings["sizeX"] = 3;
+    }
+    if (settings.find("sizeY") == settings.end()) {
+        settings["sizeY"] = 3;
+    }
+    if (settings.find("solvable") == settings.end()) {
+        settings["solvable"] = true;
+    }
+    if (settings.find("aimode") == settings.end()) {
+        settings["aimode"] = false;
+    }
+    return settings;
+}
+
+int main(int argc, const char **argv) {
     sf::RenderWindow window(sf::VideoMode(y15 * buttonSize, x15 * buttonSize), "15", 7);
     window.setFramerateLimit(60);
+
+    std::string exec = "python3 ../gen.py -s ";
+    exec += argc != 2 ? std::to_string(3) : argv[1];
+    exec += " > genValue.txt";
+    system(exec.c_str());
+    sleep(1);
+    std::ifstream file("genValue.txt", std::ios_base::in);
+    std::vector<int> mapGen;
+    if (file.is_open()) {
+        std::cout << "File open\n";
+    }
+    std::string line;
+    while (std::getline(file, line)) {
+        if (*line.begin() != '#') {
+            mapGen.emplace_back(std::stoi(line));
+        }
+    }
+    std::map<std::string, int> settings;
+    try {
+         settings = argumentSettings(argv);
+    }
+    catch(std::exception& e) {
+        std::cerr << e.what();
+        return 1;
+    }
     Game game(0, 0); // 0, 0 now is unused(screen size)
     while (window.isOpen()) {
         sf::Event windowEvent;
